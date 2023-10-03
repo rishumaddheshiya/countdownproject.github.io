bc7e99b5d08893600582e4e3b443c9319c363706
rishumaddheshiya <rishumaddheshiya999@gmail.com>
2023-10-03 12:07:55 +0530

    my js project

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const enddate = "10 Dec 2023 12:00 AM";
+
+document.getElementById("enddate").innerText=enddate;
+const inputs =document.querySelectorAll('input')
+const clock =()=>{
+    const end=new Date(enddate);
+    const now = new Date();
+
+    const diff=(end-now)/1000;
+        
+if(diff<0)
+return;
+    inputs[0].value=Math.floor(diff/86400);
+    inputs[1].value=Math.floor((diff/3600)%24);
+    inputs[2].value=Math.floor((diff/60)%60);
+    inputs[3].value=Math.floor(diff%60);
+}
+clock();
+setInterval( clock, 1000);
+
+
